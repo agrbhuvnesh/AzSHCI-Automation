fb4d38db2d23c02336589300a212fc128803f4df
Priyanshi Jain <priyjain@microsoft.com>
2023-05-17 20:00:23 +0530

    scrub PII

--- install-ama-arc-extension/scripts/install-ama.ps1
+++ install-ama-arc-extension/scripts/install-ama.ps1
@@ -3,21 +3,6 @@ $subscriptionId = ""
 $resourceGroup = ""
 $DCRFilePath = "path to DCRAssociation.json"
 
-# Connect-AzAccount -Subscription $subscriptionId
-# New-AzDataCollectionRule -Location "East US" -ResourceGroupName bkumar-test-rg -RuleName "AMATestRule" -RuleFile "C:\bhuvi\azurestack\HCI\tryout\POC\DCRAssociation.json" -Description "Test DCR Rule for AMA"  -verbose
-
-
-
-# for ($i = 0; $i -lt 2; $i++)
-# {
-# $currentCluster = $clusters[$i]
-# Write-Host ($currentCluster)
-# New-AzStackHciExtension  -ArcSettingName "default" -ClusterName $currentCluster -Name AzureMonitorWindowsAgent  -ResourceGroupName $resourceGroup -ExtensionParameterPublisher Microsoft.Azure.Monitor -ExtensionParameterType "AzureMonitorWindowsAgent"
-    
-# New-AzDataCollectionRuleAssociation -TargetResourceId "/subscriptions/$using:subscriptionId/resourceGroups/bkumar-test-rg/providers/Microsoft.AzureStackHCI/clusters/$using:currentCluster" -AssociationName "AMATestNAme$i" -RuleId /subscriptions/$using:subscriptionId/resourceGroups/$using:resourceGroup/providers/Microsoft.Insights/dataCollectionRules/AMATestRule
-# }
-
-
 
 az login --tenant $tenantID
 az account set -s  $subscriptionId
