d787d49e001b215648ad75febaffd07e3725adf4
Prateek Prasher <pprasher@microsoft.com>
2023-05-18 12:20:11 +0530

    Parameterize api version

--- toggle-extension-automatic-upgrades-with-Invoke-AzRestMethod/toggle-extension-automatic-upgrades-all-extensions.ps1
+++ toggle-extension-automatic-upgrades-with-Invoke-AzRestMethod/toggle-extension-automatic-upgrades-all-extensions.ps1
@@ -2,6 +2,7 @@ $subscriptionId = ""
 $resourceGroup = ""
 $tenantId = ""
 $enableAutomaticUpgrade = $true
+$apiVersion = "2023-03-01"
 
 Connect-AzAccount -Subscription $subscriptionId -Tenant $tenantId
 
@@ -33,7 +34,7 @@ foreach ($extension in $extensions) {
     $updatedExtension.properties.extensionParameters.enableAutomaticUpgrade = $enableAutomaticUpgrade
 
     $payload = $updatedExtension | ConvertTo-Json -Depth 10
-    $path = $extension.ResourceId + "?api-version=2023-03-01"
+    $path = $extension.ResourceId + "?api-version=" + $apiVersion
 
     Write-Host ("Setting automatic upgrades to $enableAutomaticUpgrade on extension $extension.ResourceId")
 
